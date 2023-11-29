1902fb53ef1972e4bb7dbb36929e8ad9c1c1370d
mdyner19 <michelledyner@gmail.com>
2023-11-28 21:01:41 -0500

    Added support for user profiles

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 import express from 'express';
 import cors from "cors";
+import UserRoutes from './users/routes.js';
+import mongoose from "mongoose";
+import session from "express-session";
+import "dotenv/config";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING, {
+    dbName: 'cs4550FinalProject', 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
 const app = express();
-app.use(cors());
-app.listen(4000);
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('CS4550 Final Project api')
+})
+UserRoutes(app);
+app.listen(process.env.PORT || 4000);
---
+++ users/schema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    bio: String,
+    role: {
+        type: String,
+        enum: ["ADMIN", "USER"],
+        default: "USER"
+    },
+    following: [mongoose.Schema.Types.ObjectId],
+    followers: [mongoose.Schema.Types.ObjectId]
+},
+    { collection: "users" });
+export default userSchema;
